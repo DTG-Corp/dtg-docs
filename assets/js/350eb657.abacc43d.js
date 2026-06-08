10a06a7b32bb77025672e2ca6c1c24dd043cdea9
Do Ngoc Quy <dongocquyonline@gmail.com>
2026-06-08 23:49:43 +0000

    config: DTG-Corp/dtg-docs GitHub Pages settings

---
+++ assets/js/350eb657.abacc43d.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkmy_website=self.webpackChunkmy_website||[]).push([["2115"],{2042(e){e.exports=JSON.parse('{"metadata":{"permalink":"/dtg-docs/blog","page":1,"postsPerPage":10,"totalPages":1,"totalCount":4,"blogDescription":"Blog","blogTitle":"Blog"}}')}}]);
---
+++ assets/js/acecf23e.385c80e8.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkmy_website=self.webpackChunkmy_website||[]).push([["3220"],{1912(s){s.exports=JSON.parse('{"blogBasePath":"/dtg-docs/blog","blogTitle":"Blog","authorsListPath":"/dtg-docs/blog/authors"}')}}]);
